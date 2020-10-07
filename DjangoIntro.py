6d6138e95f73358d9a538131c956ddddcc9aaba9
sivamatsa <sivaaim12345@gmail.com>
2020-10-07 22:23:25 +0530

    Django Started

---
+++ DjangoIntro.py
@@ -0,0 +1,33 @@
+'''
+Django Introduction:
+
+1. Readymade framework
+2. Python Web framework
+3. its high level MVC-MVT
+            Model View Controller    Model view template
+
+            Model: Contains All Database Part like Tables
+            VIew: Front end Part like html , css --contains All html files
+            controller: contains logics
+
+            MVT:
+            Model :Contains all Data
+            View:all logics
+            Template:all html files
+
+    Features:
+    
+3.  fast ,
+    secure ,
+    Authentication ,
+    scalable ,
+    fully loaded ,
+    open source
+
+like    Django
+        flask
+        web2py
+        bottel
+        cherrypy
+'''
+python 3.6.8
---
+++ django install.py
@@ -0,0 +1,34 @@
+'''
+pip install django
+
+if not change path for pip
+
+import django
+
+dir(django)
+['VERSION', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', '__version__', 'get_version', 'setup', 'utils']
+
+django.get_version()
+'3.1.2'
+
+
+django-admin--version
+
+for project
+
+django-admin startproject project name
+
+  editors :
+
+  sublime
+  visual studio
+  pycharm
+  atom  ..etc
+'''
+
+
+init.py : packages
+settings.py : all apps info , database
+url.py : all urls browsers related
+wsgi: web server gateway interface
+manage.py: 
